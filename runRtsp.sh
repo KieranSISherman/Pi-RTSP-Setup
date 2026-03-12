13c14da411ab9b8bf8067965c7fd243341c4c43d
kieransherman-weber <kieransherman@mail.weber.edu>
2026-03-12 16:33:17 -0600

    Fixed flag typo

--- runRtsp.sh
+++ runRtsp.sh
@@ -3,7 +3,7 @@
 source config.env
 
 EXEC_DIR="$HOME/Pi-RTSP"
-FFMPEG_CMD="ffmpeg -f v4l2 -input_format mjpeg -video_size 1280x720 -framerate 30 -i /dev/video0 -f alsa -i plughw:CARD=$CARD,DEV=$DEV -acodec aac -ar 44100 -c 2 -b:a 128k -vcodec libx264 -preset ultrafast -tune zerolatency -f rtsp -rtsp_transport tcp rtsp://localhost:8554/stream"
+FFMPEG_CMD="ffmpeg -f v4l2 -input_format mjpeg -video_size 1280x720 -framerate 30 -i /dev/video0 -f alsa -i plughw:CARD=$CARD,DEV=$DEV -acodec aac -ar 44100 -ac 2 -b:a 128k -vcodec libx264 -preset ultrafast -tune zerolatency -f rtsp -rtsp_transport tcp rtsp://localhost:8554/stream"
 
 echo "Starting RTSP Server"
 "$EXEC_DIR/mediamtx" "$EXEC_DIR/mediamtx-rtsp.yml" &
